82612e9155d68d340a4779208ae842273d96d864
gupta-anmol <anmolgupta1311@gmail.com>
2022-04-22 14:37:38 +0530

    Changes

--- backend/run.py
+++ backend/run.py
@@ -12,24 +12,13 @@ app = Flask(__name__)
 CORS(app)
 app.secret_key = "5ZN5zi!45QUsGGh"
 
+
 @app.route("/", methods=["GET", "POST"])
 def exercises():
     try:
         with open("data/data.json") as f:
             data = json.load(f)
 
-    response = []
-    
-    for equipment in request.json["equipments"]:
-        for muscle_group in request.json["muscle_groups"]:
-            print(equipment, muscle_group)
-            if equipment in data and muscle_group in data[equipment]:
-                for exercise in data[equipment][muscle_group]:
-                    item = {}
-                    item["equipment"] = equipment
-                    item["muscle_group"] = muscle_group
-                    item["exercise"] = exercise
-                    response.append(item)
         response = {}
         response["success"] = True
         response["exercises"] = []
@@ -43,27 +32,36 @@ def exercises():
                         item["equipment"] = equipment
                         item["muscle_group"] = muscle_group
                         item["exercise"] = exercise
-                        response.append(item)
+                        response["exercises"].append(item)
 
         for equipment in request.json["equipments"]:
+            if equipment in data:
+                for muscle_group in data[equipment]:
+                    if muscle_group not in request.json["muscle_groups"]:
+                        for exercise in data[equipment][muscle_group]:
+                            item = {}
+                            item["equipment"] = equipment
+                            item["muscle_group"] = muscle_group
+                            item["exercise"] = exercise
+                            response["exercises"].append(item)
 
+        return jsonify(response)
+
+    except Exception as e:
+        response["success"] = False
+        response["detail"] = "There is an error"
+        del response["exercises"]
+        return jsonify(response)
     # print(response)
-    return jsonify(response)
-            for muscle_group in data[equipment]:
-                if muscle_group not in request.json["muscle_groups"]:
-                    for exercise in data[equipment][muscle_group]:
-                        item = {}
-                        item["equipment"] = equipment
-                        item["muscle_group"] = muscle_group
-                        item["exercise"] = exercise
-                        response.append(item)
+    # return jsonify(response)
+
 
 @app.route("/imageRecognize", methods=["POST"])
 def recognize():
-    image_base64_string = dict(request.form)['image']
-    print("request file",len(image_base64_string))
+    image_base64_string = dict(request.form)["image"]
+    print("request file", len(image_base64_string))
     print(image_base64_string[:200])
-    image_base64_string = image_base64_string.replace("data:image/jpeg;base64,", "");
+    image_base64_string = image_base64_string.replace("data:image/jpeg;base64,", "")
     # image_base64_string = image_base64_string.replace("data:image/jpeg;base64,", "");
     # image_data = base64.decodebytes(bytes(image_base64_string, "utf-8"))
     # image = np.array(image_data)
@@ -72,14 +70,7 @@ def recognize():
     im.show()
     response = []
     return jsonify(response)
-        # print(response)
-
-        return jsonify(response)
-    except Exception as e:
-        response["success"] = False
-        response["detail"] = "There is an error"
-        del response["exercises"]
-        return jsonify(response)
+    # print(response)
 
 
 if __name__ == "__main__":
