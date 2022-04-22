b7f1325fcd6b967351411c1f359a09a163097d7d
gupta-anmol <anmolgupta1311@gmail.com>
2022-04-22 12:29:27 +0530

    Catch error

--- backend/run.py
+++ backend/run.py
@@ -11,35 +11,44 @@ app.secret_key = "5ZN5zi!45QUsGGh"
 
 @app.route("/", methods=["GET", "POST"])
 def exercises():
-    with open("data/data.json") as f:
-        data = json.load(f)
+    try:
+        with open("data/data.json") as f:
+            data = json.load(f)
 
-    response = []
+        response = {}
+        response["success"] = True
+        response["exercises"] = []
 
-    for equipment in request.json["equipments"]:
-        for muscle_group in request.json["muscle_groups"]:
-            print(equipment, muscle_group)
-            if equipment in data and muscle_group in data[equipment]:
-                for exercise in data[equipment][muscle_group]:
-                    item = {}
-                    item["equipment"] = equipment
-                    item["muscle_group"] = muscle_group
-                    item["exercise"] = exercise
-                    response.append(item)
+        for equipment in request.json["equipments"]:
+            for muscle_group in request.json["muscle_groups"]:
+                print(equipment, muscle_group)
+                if equipment in data and muscle_group in data[equipment]:
+                    for exercise in data[equipment][muscle_group]:
+                        item = {}
+                        item["equipment"] = equipment
+                        item["muscle_group"] = muscle_group
+                        item["exercise"] = exercise
+                        response.append(item)
 
-    for equipment in request.json["equipments"]:
-        for muscle_group in data[equipment]:
-            if muscle_group not in request.json["muscle_groups"]:
-                for exercise in data[equipment][muscle_group]:
-                    item = {}
-                    item["equipment"] = equipment
-                    item["muscle_group"] = muscle_group
-                    item["exercise"] = exercise
-                    response.append(item)
+        for equipment in request.json["equipments"]:
 
-    # print(response)
+            for muscle_group in data[equipment]:
+                if muscle_group not in request.json["muscle_groups"]:
+                    for exercise in data[equipment][muscle_group]:
+                        item = {}
+                        item["equipment"] = equipment
+                        item["muscle_group"] = muscle_group
+                        item["exercise"] = exercise
+                        response.append(item)
 
-    return jsonify(response)
+        # print(response)
+
+        return jsonify(response)
+    except Exception as e:
+        response["success"] = False
+        response["detail"] = "There is an error"
+        del response["exercises"]
+        return jsonify(response)
 
 
 if __name__ == "__main__":
