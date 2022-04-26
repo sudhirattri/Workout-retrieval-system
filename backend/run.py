93405a9dd3190bf9ed0d91f3d173f7989b793c8a
Sudhir Attri <sudhir18267@iiitd.ac.in>
2022-04-26 23:23:35 +0530

    connected resnet model

--- backend/run.py
+++ backend/run.py
@@ -6,12 +6,27 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 import os
+from tensorflow import keras
+import tensorflow as tf
+model_path = './model/model_pretrained_resnet'
+model = keras.models.load_model(model_path)
+
+def get_model_tf():
+    #Reading the model from JSON file
+    with open("./model//renet.json", 'r') as json_file:
+        json_savedModel= json_file.read()
+
+    
+    model = tf.keras.models.model_from_json(json_savedModel)
+    model.load_weights('./model//model.h5')
+    return model
+
+model_tf =get_model_tf()
 
 app = Flask(__name__)
 
 CORS(app)
 app.secret_key = "5ZN5zi!45QUsGGh"
-
 
 @app.route("/", methods=["GET", "POST"])
 def exercises():
@@ -66,15 +81,32 @@ def recognize():
     # image_data = base64.decodebytes(bytes(image_base64_string, "utf-8"))
     # image = np.array(image_data)
     im = Image.open(BytesIO(base64.b64decode(image_base64_string)))
-    print(im)
-    im.show()
+
+    prediction,probs = predict_image(im)
+    print(prediction,probs)
+    # im.show()
     response = {}
     response["success"] = True
-    response["equipment"] = "Barbell"
+    response["equipment"] = str(prediction)
+    response["probs"] = str(probs)
 
     return jsonify(response)
     # print(response)
 
+def predict_image(image):
+    image = image.resize((224,224))
+    img_array = tf.keras.preprocessing.image.img_to_array(image)
+    img_batch = np.expand_dims(img_array, axis=0)
+    arr = model_tf.predict(img_batch)
+    maxi = 0
+    cur=-1
+    for i in range (0,6):
+        if(arr[0][i]>maxi):
+            maxi = arr[0][i]
+            cur=i
+    names = ['Aerobic Steppers','Bench Press','Bicycle','Dumb Bell','Elliptical','Leg Press','Pec Deck','Rowing','Treadmill']
+
+    return names[cur],maxi
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
